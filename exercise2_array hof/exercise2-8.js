64ce04d96ce9382c25711cf2df05e1a9e9cd0ec7
moorekwon <raccoonhj33@gmail.com>
2019-10-29 20:29:13 +0900

    191029 assignment2 -array HOF / a file renamed

---
+++ exercise2_array hof/exercise2-8.js
@@ -0,0 +1,13 @@
+// 8. completed 프로퍼티의 값이 true인 요소의 갯수 구하기
+
+let todos = [
+    { id: 3, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 1, content: 'Javascript', completed: false }
+  ];
+
+function countCompletedTodos() {
+    return todos.filter(todo => todo.completed).length;
+}
+
+console.log(countCompletedTodos());
---
+++ exercise2_array hof/exercise2-9.js
@@ -0,0 +1,13 @@
+// 9. id 프로퍼티의 값 중에서 최대값 구하기
+
+let todos = [
+    { id: 3, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 1, content: 'Javascript', completed: false }
+  ];
+
+function getMaxId() {
+    return Math.max(...todos.map(todo => todo.id));
+}
+
+console.log(getMaxId());
